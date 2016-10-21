7081dd14e2deb94a7ff9b1708cf33a9d9802e098
Vu Tran <phongvu.811@gmail.com>
2016-10-21 14:48:29 +0700

    20161021

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const http = require('http');
-const port = 8080;
+const express = require('express')
+const app = express()
+const port = 8080
 
-const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end('Hello Node.js Server!');
-}
+app.get('/', (request, response) => {
+  response.send('Hello from Express!')
+})
 
-const server = http.createServer(requestHandler);
+app.listen(port, (err) => {
+  if (err) {
+	return console.log('something bad happened', err)
+  }
 
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-
-    console.log(`server is listening on ${port}`)
+  console.log(`server is listening on ${port}`)
 })
