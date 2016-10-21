45851858f354b1dbc534c3481daac06f59725e11
Vu Tran <phongvu.811@gmail.com>
2016-10-21 14:12:14 +0700

    20161021

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const requestHandler = (request, response) => {
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
-  if (err) {
-	return console.log('something bad happened', err);
-  }
+    if (err) {
+        return console.log('something bad happened', err)
+    }
 
-  console.log(`server is listening on ${port}`);
+    console.log(`server is listening on ${port}`)
 })
