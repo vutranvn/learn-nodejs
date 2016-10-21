c28e9da3e49b3965d7988d7158e74c47b999c950
Vu Tran <phongvu.811@gmail.com>
2016-10-21 17:12:27 +0700

    20161021

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 8080
+var express = require('express');
+var app     = express();
+var path 	= require('path')
+var exphbs 	= require('express-handlebars')
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (request, response) => {
-  response.send('Hello from Express!')
-})
+  response.render('home', {
+    name: 'John'
+  })
+});
 
-app.listen(port, (err) => {
-  if (err) {
-	return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+app.listen(8080);
